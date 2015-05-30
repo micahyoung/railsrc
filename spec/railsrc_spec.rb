9c7eabd69da932640eff4a0d8920ef505e2b7b55
Micah Young <micah@young.io>
2015-05-30 15:24:18 -0400

    Fix relative path to RailsRC

--- spec/railsrc_spec.rb
+++ spec/railsrc_spec.rb
@@ -4,16 +4,17 @@ describe 'railsrc' do
   it 'generates a new app' do
     Dir.mktmpdir do |dir|
       rails_dir = "#{dir}/foo"
+      railsrc_path = File.join(File.dirname(__FILE__), "../railsrc")
 
-      system(<<-SH)
-        rails new "#{rails_dir}" --rc=./railsrc --database=sqlite3
+      expect(system(<<-SH)).to eql true
+        rails new "#{rails_dir}" --rc=#{railsrc_path} --database=sqlite3
         cd "#{rails_dir}"
         bundle install
         rails g controller Bar index
         rails g model Baz name:string
         rake db:migrate
-        rspec spec/controllers/bar_controller_spec.rb
-        rspec spec/models/baz_spec.rb
+        rspec spec/controllers/bar_controller_spec.rb \
+              spec/models/baz_spec.rb
       SH
     end
   end
